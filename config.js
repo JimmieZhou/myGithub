209e3cf69463852eda234d2bcb9d6521b0c324de
周 <jingyuan@zhoudeMacBook-Pro.local>
2019-10-18 17:04:00 +0800

    封装一个同构的请求方法

--- config.js
+++ config.js
@@ -4,9 +4,9 @@
  * @Author: jimmiezhou
  * @Date: 2019-10-15 17:29:48
  * @LastEditors: jimmiezhou
- * @LastEditTime: 2019-10-17 15:37:31
+ * @LastEditTime: 2019-10-18 16:46:08
  */
-
+const GITHUB_BASE_URL = "https://api.github.com";
 const GITHUB_OAUTH_URL = "https://github.com/login/oauth/authorize";
 const SCOPE = "user";
 const client_id = "b50f64b8e843c7f47137";
@@ -17,6 +17,7 @@ module.exports = {
     client_secret: "eb3190317929216cb39fc1928f00c218f931cf28",
     request_token_url: "https://github.com/login/oauth/access_token"
   },
+  GITHUB_BASE_URL,
   GITHUB_OAUTH_URL,
   OAUTH_URL: `${GITHUB_OAUTH_URL}?client_id=${client_id}&scope=${SCOPE}`
 };
