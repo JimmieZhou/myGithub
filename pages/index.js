996f079f8fdbd07399d2e5942c4d3755e02197ed
周 <jingyuan@zhoudeMacBook-Pro.local>
2019-10-18 11:42:26 +0800

    首页完成是否登录判断

--- pages/index.js
+++ pages/index.js
@@ -4,21 +4,66 @@
  * @Author: jimmiezhou
  * @Date: 2019-10-16 17:23:47
  * @LastEditors: jimmiezhou
- * @LastEditTime: 2019-10-18 10:37:33
+ * @LastEditTime: 2019-10-18 11:34:37
  */
 import api from "../lib/api";
-const Index = () => <span>Index</span>;
+import { connect } from "react-redux";
+import { Button } from "antd";
+import getConfig from "next/config";
 
-Index.getInitialProps = async ({ ctx }) => {
-  const result = await api.request(
+const { publicRuntimeConfig } = getConfig();
+
+const Index = ({ userRepos, userStarredRepos, user }) => {
+  if (!user || !user.id) {
+    return (
+      <div className="root">
+        <p>亲，您还没有登录哦~</p>
+        <Button type="primary" href={publicRuntimeConfig.OAUTH_URL}>
+          点击登录
+        </Button>
+        <style jsx>{`
+          .root {
+            height: 400px;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+          }
+        `}</style>
+      </div>
+    );
+  }
+  return <span>Index</span>;
+};
+
+Index.getInitialProps = async ({ ctx, reduxStore }) => {
+  const user = reduxStore.getState().user;
+  if (!user || !user.id) {
+    return {
+      isLogin: false
+    };
+  }
+  const userReposResp = await api.request(
     {
-      url: "/search/repositories?q=react"
+      url: "/user/repos"
+    },
+    ctx.req
+  );
+  const userStarredReposResp = await api.request(
+    {
+      url: "/user/starred"
     },
     ctx.req
   );
   return {
-    data: result.data
+    isLogin: true,
+    userRepos: userReposResp.data,
+    userStarredRepos: userStarredReposResp.data
   };
 };
 
-export default Index;
+export default connect(function mapStateToProps(state) {
+  return {
+    user: state.user
+  };
+})(Index);
