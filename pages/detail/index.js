4539d5865fd56a233a8ca760615a3e071dec9e96
周 <jingyuan@zhoudeMacBook-Pro.local>
2019-10-21 18:06:50 +0800

    详情页面新增md文件展示

---
+++ pages/detail/index.js
@@ -0,0 +1,41 @@
+/*
+ * @Descripttion: 详情页
+ * @version: 1.0.0
+ * @Author: jimmiezhou
+ * @Date: 2019-10-17 14:43:27
+ * @LastEditors: jimmiezhou
+ * @LastEditTime: 2019-10-21 17:01:39
+ */
+
+import dynamic from "next/dynamic";
+
+import withRepoBasic from "../../components/with-repo-basic";
+import api from "../../lib/api";
+
+const MDRenderer = dynamic(() => import("../../components/MarkdownRenderer"));
+
+function Detail({ readme }) {
+  return <MDRenderer content={readme.content} isBase64={true} />;
+}
+
+Detail.getInitialProps = async ({
+  ctx: {
+    query: { owner, name },
+    req,
+    res
+  }
+}) => {
+  const readmeResp = await api.request(
+    {
+      url: `/repos/${owner}/${name}/readme`
+    },
+    req,
+    res
+  );
+
+  return {
+    readme: readmeResp.data
+  };
+};
+
+export default withRepoBasic(Detail, "index");
