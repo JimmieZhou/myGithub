8fd953b363c2a097d45d837ed4fc0e5003c8b871
JimmieZhou <jimmiezhou@126.com>
2019-12-09 21:19:46 +0800

    修改github client_secret

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
  * @Author: jimmiezhou
  * @Date: 2019-10-15 17:29:48
  * @LastEditors: jimmiezhou
- * @LastEditTime: 2019-10-25 10:30:49
+ * @LastEditTime: 2019-12-09 15:43:19
  */
 const GITHUB_BASE_URL = "https://api.github.com";
 const GITHUB_OAUTH_URL = "https://github.com/login/oauth/authorize";
@@ -15,7 +15,7 @@ const client_id = "b50f64b8e843c7f47137";
 module.exports = {
   github: {
     client_id,
-    client_secret: "eb3190317929216cb39fc1928f00c218f931cf28",
+    client_secret: "69c420a13948fd3a4025c639e272a811d4b92f42",
     request_token_url: "https://github.com/login/oauth/access_token"
   },
   GITHUB_BASE_URL,
